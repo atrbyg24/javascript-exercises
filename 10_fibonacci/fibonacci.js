593d2a87e59b19219d57b13a7cb7ab71e5377411
atrbyg24 <artwan520@gmail.com>
2025-01-11 09:49:42 -0500

    Complete exercises 08-12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (isNaN(n)) {
+        return "OOPS";
+    }
+    if (n < 0) {
+        return "OOPS";
+    }
+    let a = 0
+    let b = 1
+    for (let i = 0; i < n;i++) {
+        let temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return a;
 };
 
 // Do not edit below this line
